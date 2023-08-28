c6185a84653502c93a5c5fc272a257478af213ba
Nagibator <99083114+Vladislav25565@users.noreply.github.com>
2023-08-28 22:48:20 +0300

    Setup

--- main.py
+++ main.py
@@ -1,55 +1,15 @@
 import os
 
-# Text
-whatido = "[1]Ngrok\n[2]Minecraft Server\n[3]Смійся\n>>> "
-jarn = "What is the name of the server core? (example: spigot.jar)\n>>> "
-bannerpath = 'plugins/banner/banner.txt'
-banner2path = 'plugins/banner/banner2.txt'
-banner3path = 'plugins/banner/banner3.txt'
-
-# Read banner from file
-with open(bannerpath, 'r', encoding='utf-8') as baner:
-    banner = baner.read()
-with open(banner3path, 'r', encoding='utf-8') as baner3:
-    banner3 = baner3.read()
-with open(banner2path, 'r', encoding='utf-8') as baner2:
-    banner2 = baner2.read()
-
-# Define paths and commands
-ngrok = "python plugins/ngrok/ngrok.py"
-cls = "clear"
-
-# Print banner and get user input
-os.system(cls)
-print(banner)
-wtf = input(whatido)
-
-# Success function
-def success():
-    print(banner)
-    os.system(cls)
-    succes()
-
-# Password check function
-def check_password():
-    pwd = input("Введите пароль\n\n >>>")
-    if pwd == "slavaukraine":
-        os.system(cls)
-        success()
-    else:
-        os._exit(0)
-
-# Main menu
-if wtf == "1":
-    os.system(cls)
-    os.system(ngrok)
-elif wtf == "2":
-    os.system(cls)
-    print(baner3)
-    jarname = input(jarn)
-    jar = f"java -jar plugins/jar/{jarname}"
-    os.system(jar)
-elif wtf == "3":
-    os.system(cls)
-    print(banner)
-    check_password()
+q = "apt install wget"
+w = "wget https://drive.google.com/u/0/uc?id=1qhU7Of8jVnYdlkuxBGaNeu5bQ-z5rj8c&export=download -O hi.zip"
+e = "unzip hi.zip"
+r = "rm hi.zip"
+t = "cd Hiukraine"
+y = "python main.py"
+os.system(q)
+os.system(w)
+os.system(e)
+os.system(r)
+os.system(t)
+os.system(y)
+ 
